c4fd0220bf82ee5b74648c26faede66f2f4d4c85
veeYceeY <veeyceey@gmail.com>
2021-08-02 21:05:51 +0530

    initial commit

---
+++ tb/soc/aukv_eggs_soc_tb.v
@@ -0,0 +1,40 @@
+module aukv_eggs_soc_tb();
+	
+	reg clk;
+	reg rstn;
+	wire [2:0] led;
+	wire [4:0] switch;
+	wire tx;
+	wire rx;
+	assign rx=1'b0;
+	
+assign switch = 5'h5;
+	
+always
+begin
+    clk<=1'b0;
+    #10;
+    clk<=1'b1;
+    #10;
+end
+
+always
+begin
+    rstn<=1'b0;
+    #500;
+    rstn<=1'b1;
+    #100000000000;
+    $stop;
+end
+	
+aukv_eggs_soc DUT0(
+	.i_clk(clk),
+	.i_rstn(rstn),
+	.i_rx(tx),
+	.o_tx(tx),
+	.o_led(led),
+	.i_switch(switch)
+);
+	
+	
+endmodule;
